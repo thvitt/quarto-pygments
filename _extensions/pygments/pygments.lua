bd2eabb174e2e3225ac89d4bded9a74e4ff8f5f8
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2026-05-02 18:10:17 +0200

    Actually implemented version, reveal only.

--- _extensions/pygments/pygments.lua
+++ _extensions/pygments/pygments.lua
@@ -1,6 +1,62 @@
+local default_inline_lang = nil
 
--- Reformat all heading text 
-function Header(el)
-  el.content = pandoc.Emph(el.content)
-  return el
+function Meta(meta)
+	if meta["inline-code-lang"] then
+		default_inline_lang = pandoc.utils.stringify(meta["inline-code-lang"])
+	end
+	return meta
 end
+
+function Code(el)
+	local lang = el.classes[1] or default_inline_lang
+	if lang == nil or lang == "" or lang == "text" then
+		return el
+	end
+
+	local success, res = pcall(pandoc.pipe, "pygmentize", { "-l", lang, "-f", "html", "-O", "nowrap=True" }, el.text)
+	if success then
+		-- Remove trailing newline/whitespace
+		res = res:gsub("%s+$", "")
+		return pandoc.RawInline("html", '<code class="sourceCode ' .. lang .. '">' .. res .. "</code>")
+	else
+		return el
+	end
+end
+
+function CodeBlock(el)
+	-- Only process if a language is specified
+	if el.classes[1] == nil then
+		return el
+	end
+
+	-- Define the language and the code
+	local lang = el.classes[1]
+	local code = el.text
+
+	-- Run pygmentize: -l (language), -f (format), -O (options)
+	-- 'nowrap=True' is used so we can wrap it in Reveal.js compatible tags later if needed
+	local success, res = pcall(pandoc.pipe, "pygmentize", { "-l", lang, "-f", "html", "-O", "nowrap=True" }, code)
+
+	if success then
+		-- Wrap in standard Reveal.js/Quarto classes so the layout doesn't break
+		local html = '<div class="sourceCode"><pre class="sourceCode '
+			.. lang
+			.. '"><code class="sourceCode '
+			.. lang
+			.. '">'
+			.. res
+			.. "</code></pre></div>"
+		return pandoc.RawBlock("html", html)
+	else
+		-- Fallback to default if pygmentize fails
+		return el
+	end
+end
+
+return {
+	{ Meta = Meta },
+	{
+		Code = Code,
+		CodeBlock = CodeBlock,
+	},
+}
